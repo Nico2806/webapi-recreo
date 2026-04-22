e3dc1125bfb29448e569a146ab92b76ddc2271bc
Nico2806 <nico.properzi@gmail.com>
2026-04-21 23:14:57 -0300

    fix connection string render

--- Program.cs
+++ Program.cs
@@ -17,7 +17,10 @@ builder.Services.AddCors(options =>
 
 // DB
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? builder.Configuration["DefaultConnection"]
+));
 
 // Controllers
 builder.Services.AddControllers();
