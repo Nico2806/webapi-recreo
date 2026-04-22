aa07371e02bdb9f2ce6397e7a82a5a2a8c65a5da
Nico2806 <nico.properzi@gmail.com>
2026-04-21 22:14:39 -0300

    fix nulls en modelo

--- Models/Pedido.cs
+++ Models/Pedido.cs
@@ -4,17 +4,17 @@
     {
         public int Id { get; set; }
 
-        public string NombreArchivo { get; set; }
-        public string RutaArchivo { get; set; }
+        public string? NombreArchivo { get; set; }
+        public string? RutaArchivo { get; set; }
 
-        public string TipoImpresion { get; set; } // Color / B&N
-        public string Tamano { get; set; } // A4, A3
+        public string? TipoImpresion { get; set; } // Color / B&N
+        public string? Tamano { get; set; } // A4, A3
         public int Cantidad { get; set; }
         public bool Anillado { get; set; }
 
         public decimal PrecioTotal { get; set; }
 
-        public string Estado { get; set; } = "Pendiente";
+        public string? Estado { get; set; } = "Pendiente";
 
         public DateTime Fecha { get; set; } = DateTime.Now;
 
